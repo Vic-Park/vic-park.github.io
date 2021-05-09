42f79c8b231d50c35f0b3d6a4fe886573f2d4ae2
Leon Si <leonzalion@gmail.com>
2021-05-08 21:43:29 -0400

    deploy

---
+++ precache-manifest.b3f36ec6e3727459967a3685e4e5a782.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b548445ed02b33934e",
+    "url": "/css/app.db0ce19c.css"
+  },
+  {
+    "revision": "08310f8904208060a536",
+    "url": "/css/chunk-vendors.67d21c54.css"
+  },
+  {
+    "revision": "b065eb928e2f5128bba6",
+    "url": "/css/events.6705ba59.css"
+  },
+  {
+    "revision": "8512233560fee7707a331cade0abb390",
+    "url": "/img/vic-park-logo.png"
+  },
+  {
+    "revision": "c3dff18035a8dc729f15040b65889dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eda697035ac24221d6d",
+    "url": "/js/404.eca937bd.js"
+  },
+  {
+    "revision": "28b548445ed02b33934e",
+    "url": "/js/app.2b72e47b.js"
+  },
+  {
+    "revision": "08310f8904208060a536",
+    "url": "/js/chunk-vendors.c051797b.js"
+  },
+  {
+    "revision": "17f7d91d037aabbbe675",
+    "url": "/js/club.d95e6fd2.js"
+  },
+  {
+    "revision": "6181f9d01e67fdb09931",
+    "url": "/js/clubs-list.27364a74.js"
+  },
+  {
+    "revision": "f090aee0ffe16f756ab0",
+    "url": "/js/event.fa13ccc1.js"
+  },
+  {
+    "revision": "b065eb928e2f5128bba6",
+    "url": "/js/events.ad2b0dd1.js"
+  },
+  {
+    "revision": "f75983a70c31b4135262",
+    "url": "/js/home.cae7d25e.js"
+  },
+  {
+    "revision": "cd6b3519ab9ad02f813391cf2aa4f229",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
