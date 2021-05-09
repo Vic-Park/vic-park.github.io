cbea62bf001f1930f11f80834f1ffe92512ba805
Leon Si <leonzalion@gmail.com>
2021-05-08 21:43:03 -0400

    better header

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="sticky top-0 flex flex-row justify-between p-4 bg-gray-100">
-    <router-link to="/" active-class="text-blue-400">
+  <div class="sticky top-0 flex flex-row justify-between p-4 bg-gray-100 z-50">
+    <router-link to="/" active-class="text-blue-400" class="p-4">
       <span class="text-lg font-bold hover:text-blue-400">VPCI Clubs</span>
     </router-link>
     <div class="absolute left-1/2">
       <div style="margin-left: -50%">
         <img
-          class="h-16 sm:h-28 border-2 border-gray-300 rounded-full shadow-lg"
+          class="border-2 border-gray-300 rounded-full transition-height"
+          :class="[isLargeLogo ? 'h-16 sm:h-28 shadow-lg' : 'h-16']"
           src="/img/vic-park-logo.png"
         />
       </div>
     </div>
-    <div class="flex flex-row gap-x-4">
+    <div class="flex flex-row gap-x-4 p-4">
       <NavigationHeaderLink to="/clubs" title="Club List" />
       <NavigationHeaderLink to="/events" title="Events" />
     </div>
@@ -20,12 +21,32 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 
 import NavigationHeaderLink from './NavigationHeaderLink.vue';
 
 export default defineComponent({
   name: 'NavigationHeader',
   components: { NavigationHeaderLink },
+  setup() {
+    const isLargeLogo = ref(true);
+
+    function onScroll() {
+      if (window.top.scrollY > 0) {
+        isLargeLogo.value = false;
+      } else {
+        isLargeLogo.value = true;
+      }
+    }
+
+    onMounted(() => {
+      onScroll();
+      window.addEventListener('scroll', onScroll);
+    });
+
+    return {
+      isLargeLogo,
+    };
+  },
 });
 </script>
