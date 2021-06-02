2b0c85e25c3c644b474bf63807152b505f012f8b
Leon Si <leonzalion@gmail.com>
2021-06-02 14:11:55 -0400

    heroku fix

---
+++ types/entry.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EntryType = void 0;
+var EntryType;
+(function (EntryType) {
+    EntryType["club"] = "club";
+    EntryType["announcement"] = "announcement";
+    EntryType["event"] = "event";
+})(EntryType = exports.EntryType || (exports.EntryType = {}));
