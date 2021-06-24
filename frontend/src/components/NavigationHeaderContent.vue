3e177ecb90cf8993a0ab8950a04c8adb29baef74
Leon Si <leonzalion@gmail.com>
2021-06-24 13:20:57 -0400

    fix tab spacing

--- frontend/src/components/NavigationHeaderContent.vue
+++ frontend/src/components/NavigationHeaderContent.vue
@@ -1,15 +1,14 @@
 <template>
   <NavigationHeaderLink class="mx-2 mr-auto" route="/" title="home" />
-  <div class="hidden md:flex flex-1 flex-row gap-x-4 justify-end ">
-    <NavigationHeaderLink
-      class="mx-2"
-      v-for="tab in tabs"
-      :key="tab.title"
-      :route="tab.route"
-      :toId="tab.toId"
-      :title="tab.title"
-      :class="tab.class"
-    />
+  <div class="hidden md:flex flex-1 flex-row gap-x-4 justify-end">
+    <div v-for="tab in tabs" :key="tab.title" class='mx-2'>
+      <NavigationHeaderLink
+        :route="tab.route"
+        :toId="tab.toId"
+        :title="tab.title"
+        :class="tab.class"
+      />
+    </div>
   </div>
   <div class="md:hidden">
     <vue-icon :icon="mdiMenu" class="text-white h-full mr-2" size="20px" @click="onMenuClick" />
@@ -17,7 +16,7 @@
       <div class="flex flex-col absolute right-0 border rounded-sm overflow-hidden bg-white p-1">
         <div v-for="tab in tabs" :key="tab.title">
           <NavigationHeaderLink
-            textClass='p-2'
+            textClass="p-2"
             class="text-burgundy"
             :key="tab.title"
             :route="tab.route"
