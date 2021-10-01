cbb0e9b9bdd816d7a592a50b6ed6f78fef75be89
Leon Si <leonzalion@gmail.com>
2021-09-30 22:47:13 -0400

    tweak script

--- scripts/compress-images.ts
+++ scripts/compress-images.ts
@@ -6,12 +6,13 @@ import imageminMozjpeg from 'imagemin-mozjpeg';
 import imageminOptipng from 'imagemin-optipng';
 import imageminPngquant from 'imagemin-pngquant';
 import path from 'path';
+import resizeImg from 'resize-img';
 
 const imageAssetsPath = path.join(__dirname, '../assets/images');
 const publicImagesPath = path.join(__dirname, '../frontend/public/images');
 
 export async function compressImages() {
-	fs.mkdirSync(publicImagesPath, { recursive: true });
+	await fs.promises.mkdir(publicImagesPath, { recursive: true });
 
 	const imagePaths = await globby([`${imageAssetsPath}/**/*.{jpg,png}`]);
 
@@ -21,12 +22,31 @@ export async function compressImages() {
 				publicImagesPath,
 				path.relative(imageAssetsPath, path.dirname(imagePath))
 			);
+			await fs.promises.mkdir(destImageFolderPath, { recursive: true });
 			const destImageFilePath = path.join(
 				destImageFolderPath,
 				path.basename(imagePath)
 			);
+
 			if (fs.existsSync(destImageFilePath)) return;
-			await imagemin([imagePath], {
+
+			let srcImageFilePath;
+			if (imagePath.includes('club-icons')) {
+				// Copy a resized version of the image to the public/ folder
+				await fs.promises.writeFile(
+					destImageFilePath,
+					await resizeImg(await fs.promises.readFile(imagePath), {
+						height: 400,
+						width: 400,
+					})
+				);
+				srcImageFilePath = destImageFilePath;
+			} else {
+				srcImageFilePath = imagePath;
+			}
+
+			// Optimize the resized image
+			await imagemin([srcImageFilePath], {
 				destination: destImageFolderPath,
 				plugins: [
 					imageminJpegtran(),
