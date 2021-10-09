c3cb9758f6986d188df1f6d7326f3f64d5315cb1
Leon Si <leonzalion@gmail.com>
2021-10-09 12:18:31 -0400

    animate explore button

--- frontend/src/components/explore-button.vue
+++ frontend/src/components/explore-button.vue
@@ -1,7 +1,16 @@
 <template>
 	<router-link
 		w:p="y-3 x-5"
-		class="z-10 bg-yellow text-white font-bold"
+		class="
+			z-10
+			bg-yellow
+			text-white
+			font-bold
+			transition-all
+			transform
+			hover:scale-110
+			hover:rounded-md
+		"
 		to="/clubs"
 	>
 		explore
